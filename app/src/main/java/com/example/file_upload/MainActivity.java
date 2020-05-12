f7ff8f595feffe0ef38346df0957f92bc1e052d9
Sandip-android16 <62545767+Sandip-android16@users.noreply.github.com>
2020-05-12 19:48:36 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/file_upload/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.file_upload;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Button open,upload;
+    TextView name;
+
+
+    private int PICK_PDF_REQUEST = 1;
+
+    //storage permission code
+    private static final int STORAGE_PERMISSION_CODE = 123;
+
+
+    //Uri to store the image uri
+    private Uri filePath;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+            open=findViewById(R.id.opne);
+            upload=findViewById(R.id.upload);
+            name=findViewById(R.id.file);
+
+
+
+            open.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent();
+                    intent.setType("application/pdf");
+                    intent.setAction(Intent.ACTION_GET_CONTENT);
+                    startActivityForResult(Intent.createChooser(intent, "Select Pdf"), PICK_PDF_REQUEST);
+                }
+            });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_PDF_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            filePath = data.getData();
+            String uri=filePath.toString();
+            name.setText(uri);
+        }
+    }
+}
